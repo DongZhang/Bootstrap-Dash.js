f8b46666de3a256b1d5056edb701d5a1ab38531a
DongZhang <zhangdongopq@gmail.com>
2014-10-31 02:04:50 -0400

    using chart.js to generate charts

--- js/dash.js
+++ js/dash.js
@@ -31,4 +31,70 @@ $(function () {
         $(nav).removeClass('nav-open').addClass('nav-closed');
         $(content).removeClass('cont-open').addClass('cont-closed');
     }
+
+    var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
+
+    var barChartData = {
+        labels : ["January","February","March","April","May","June","July"],
+        datasets : [
+            {
+                fillColor : "rgba(220,220,220,0.5)",
+                strokeColor : "rgba(220,220,220,0.8)",
+                highlightFill: "rgba(220,220,220,0.75)",
+                highlightStroke: "rgba(220,220,220,1)",
+                data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+            },
+            {
+                fillColor : "rgba(151,187,205,0.5)",
+                strokeColor : "rgba(151,187,205,0.8)",
+                highlightFill : "rgba(151,187,205,0.75)",
+                highlightStroke : "rgba(151,187,205,1)",
+                data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+            }
+        ]
+
+    }
+
+    var pieData = [
+        {
+            value: 300,
+            color:"#F7464A",
+            highlight: "#FF5A5E",
+            label: "Red"
+        },
+        {
+            value: 50,
+            color: "#46BFBD",
+            highlight: "#5AD3D1",
+            label: "Green"
+        },
+        {
+            value: 100,
+            color: "#FDB45C",
+            highlight: "#FFC870",
+            label: "Yellow"
+        },
+        {
+            value: 40,
+            color: "#949FB1",
+            highlight: "#A8B3C5",
+            label: "Grey"
+        },
+        {
+            value: 120,
+            color: "#4D5360",
+            highlight: "#616774",
+            label: "Dark Grey"
+        }
+
+    ];
+
+    window.onload = function () {
+        var ctx = document.getElementById("chart1").getContext("2d");
+        window.myBar = new Chart(ctx).Bar(barChartData, {
+            responsive : false
+        });
+        var ctx = document.getElementById("chart2").getContext("2d");
+        window.myPie = new Chart(ctx).Pie(pieData);
+    };
 });
