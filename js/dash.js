fdd2844639bda6f1f61692299d91ec618826a7fe
Dong <zhangdongopq@gmail.com>
2014-11-04 18:28:15 -0500

    new charts library - c3

--- js/dash.js
+++ js/dash.js
@@ -106,4 +106,32 @@ $(function () {
         var ctx = document.getElementById("chart2").getContext("2d");
         window.myPie = new Chart(ctx).Pie(pieData);
     };
+
+
+    var chart3 = c3.generate({
+        bindto: '#chart3',
+        data: {
+            columns: [
+                ['data1', 30, 200, 100, 400, 150, 250],
+                ['data2', 50, 20, 10, 40, 15, 25]
+            ],
+            type: 'bar',
+        },
+        bar: {
+            width: {
+                ratio: 0.5
+            }
+        }
+    });
+
+    var chart4 = c3.generate({
+        bindto: '#chart4',
+        data: {
+            columns: [
+                ['data1', 30],
+                ['data2', 120],
+            ],
+            type: 'pie',
+        }
+    })
 });
